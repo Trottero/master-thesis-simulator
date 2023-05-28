f2920d7fb4feb68ec82a7b1340ea76c4e5f154c8
Trottero <wrlxs@hotmail.com>
2023-05-28 17:06:40 +0200

    Add decimal inspector

---
+++ DOTSSimulator/Assets/Scripts/Editor/Internal/Unity.Entities.UI/DecimalInspector.cs
@@ -0,0 +1,31 @@
+﻿using JetBrains.Annotations;
+using UnityEngine.UIElements;
+using Unity.Entities.UI;
+using Unity.Properties;
+
+namespace Editor.Internal.Unity.Entities.UI
+{
+    [UsedImplicitly]
+    internal class DecimalInspector : PropertyInspector<decimal>
+    {
+        private TextField field;
+        
+        static DecimalInspector()
+        {
+            TypeConversion.Register((ref decimal v) => v.ToString());
+            TypeConversion.Register((ref string v) => decimal.Parse(v));
+        }
+        
+        
+        public override VisualElement Build()
+        {
+            field = new TextField(DisplayName) { value = Target.ToString() };
+            return field;
+        }
+        
+        public override void Update()
+        {
+            field.SetValueWithoutNotify(Target.ToString());
+        }
+    }
+}
